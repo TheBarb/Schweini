f6a3cd7807fd445f40297d35f763989de8e45dd1
mail@studio7berlin.de <mailstudio7berlin.de@mailstudio7berlindes-MacBook-Pro.local>
2020-08-25 17:08:29 +0200

    Jans Vorschlag, leider nicht richtig eingebunden

---
+++ functions.js
@@ -0,0 +1,55 @@
+
+let eventTarget = document.getElementById('schweini');
+
+eventTarget.onclick = function dress(){}
+
+/*Jan:
+let pictures = ['images/nackt.jpg','images/t-shirt.jpg', 'images/hose.jpg', 'images/blumejpg', 'images/hose-aus.jpg','images/t-shirt-aus','images/nackt','images/popo.jpg'];
+
+function getNextPicture(){
+  let currentImageIndex = 0;
+  function getNext() {
+  currentImageIndex++
+} return (currentImageIndex + 1) % (pictures.length)
+};
+
+getNextPicture(pictures);*/
+
+
+    /*    if (eventTarget.getAttribute("src").match('images/nackt.jpg')) {
+        return eventTarget.setAttribute("src", 'images/t-shirt.jpg')
+    } else if (eventTarget.getAttribute("src").match('images/t-shirt.jpg')) {
+        return eventTarget.setAttribute("src", 'images/hose.jpg')
+    } else if (eventTarget.getAttribute("src").match('images/hose.jpg')) {
+        return eventTarget.setAttribute("src", 'images/blume.jpg')
+    } else if (eventTarget.getAttribute("src").match('images/blume.jpg')) {
+        return eventTarget.setAttribute("src", 'images/hose-aus.jpg')
+    } else if (eventTarget.getAttribute("src").match('images/hose-aus.jpg')) {
+        return eventTarget.setAttribute("src", 'images/t-shirt-aus.jpg')
+    } else if (eventTarget.getAttribute("src").match('images/t-shirt-aus.jpg')) {
+        return eventTarget.setAttribute("src", 'images/nackt-aus.jpg')
+    } else if (eventTarget.getAttribute("src").match('images/nackt-aus.jpg')) {
+        return eventTarget.setAttribute("src", 'images/nee.jpg')
+    } else if (eventTarget.getAttribute("src").match('images/nee.jpg')) {
+        return eventTarget.setAttribute("src", 'images/popo.jpg')
+    } else if (eventTarget.getAttribute("src").match('images/popo.jpg')) {
+        return eventTarget.setAttribute("src", 'images/nackt.jpg')
+    } 
+};*/
+    
+
+
+
+
+
+/*function transformation(){
+    let newImage = document.getElementById('schweini');
+    if (newImage.src.match("nackt.jpg")) {
+    return newImage.src="images/t-shirt.jpg";
+    } else {
+        return newImage.src="images/nackt.jpg";
+    }
+}*/
+    
+    
+    
