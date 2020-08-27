ae538750d6b2e5330e86c673ae010447740fa0b8
mail@studio7berlin.de <mailstudio7berlin.de@mailstudio7berlindes-MacBook-Pro.local>
2020-08-27 12:11:04 +0200

    remove margins and console.logs

--- functions.js
+++ functions.js
@@ -1,12 +1,11 @@
 
 let eventTarget = document.getElementById('schweini');
-console.log(eventTarget)
+
 
 let pictureIndex = 0;
 eventTarget.onclick = function getNextPicture(){
     pictureIndex++
     pictureIndex = (pictureIndex) % (pictures.length)
-    console.log(pictureIndex);
     return eventTarget.setAttribute("src", pictures[pictureIndex])
 }
 
